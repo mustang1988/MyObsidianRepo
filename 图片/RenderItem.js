7bfbbba80802888030a85e3a7e837fc334e809ac
mustang1988 <mustang1988@126.com>
2022-09-16 17:41:52 +0800

    refactor: refactor item render script function

--- 图片/RenderItem.js
+++ 图片/RenderItem.js
@@ -1,43 +1,77 @@
 
 // 获取图标图片的完整路径
-const _GetImagePath = (item) => `${this.app.vault.adapter.basePath}/${item.Icon.path}`;
+const _GetImagePath = (item, fields) => item[fields['icon']] 
+                                          ? `${this.app.vault.adapter.basePath}/${item[fields['icon']].path}`
+                                          : '';
+
 // 获取显示在名称后的数量
-const _GetNum = (item, display_single) => display_single 
-                                          ? ' x '+ item.Num 
-                                          : item.Num > 1 
-                                             ? ' x ' + item.Num 
-                                             : '';
+const _GetNum = (item, display_num, fields) => item[fields['num']] && parseInt(item[fields['num']]) > display_num
+                                                ? ' x ' + parseInt(item[fields['num']])
+                                                : '';
+
 // 根据参数构造需要渲染的HTML元素
-const _BuildItemHTML = (item, width, display_single) => `<img width="${width}" src="${_GetImagePath(item)}"/>${item.Name}${_GetNum(item, display_single)}`;
+const _BuildItemHTML = (item, { width, display_num, fields} ) => {
+   // 图标, 名称, 数量均有效
+   if(_GetImagePath(item, fields) && item[fields['name']] && _GetNum(item, display_num, fields)) {
+      return `<img width="${width}" src="${_GetImagePath(item, fields)}"/>${item[fields['name']]||''}${_GetNum(item, display_num, fields)}`;
+   } else if(_GetImagePath(item, fields) && item[fields['name']]) {
+      // 数量无效, 不显示数量
+      return `<img width="${width}" src="${_GetImagePath(item, fields)}"/>${item[fields['name']]}`;
+      // 图标无效, 不显示图标
+   } else if(item[fields['name']] && _GetNum(item, display_num, fields)) {
+      return `${item[fields['name']]}${_GetNum(item, display_num, fields)}`;
+   } else {
+      return '';
+   }
+}
 
 /**
  * 渲染多个物品
  * @params {Array} items 需要渲染的物品
  * @params {Number} width 图标图片尺寸, 默认值: 15
- * @params {Boolean} display_single 是否显示数量1的数量值, 默认值: true
+ * @params {Number} display_num 显示数量的下限, 大于次值的数量会显示, 默认值: 1
  * @params {Boolean} inline 多个物品是否输出到一行, 默认值: false
  * @params {String} seperator 多个物品输出到一行时使用的分隔符, 默认值: " , "
  */
-const Renderitems = async (items, width, display_single, inline, separator) => inline
-   ? dv.paragraph(items.map(item => _BuildItemHTML(item, width, display_single)).join(separator))
-   : items.forEach(item => dv.paragraph(_BuildItemHTML(item, width, display_single)));
+const Renderitems = async (items, options) => options.inline
+   ? dv.paragraph(items.map(item => _BuildItemHTML(item, options))
+      .filter(item => item !== '')
+      .join(options.separator))
+   : items.forEach(item => dv.paragraph(_BuildItemHTML(item, options)));
 
 /**
  * 渲染单个物品
  * @params {Object} item 需要渲染的物品
  * @params {Number} width 图标图片尺寸, 默认值: 15
+ * @params {Number} display_num 显示数量的下限, 大于次值的数量会显示, 默认值: 1
  */
-const RenderItem = async (item, width, display_single) => dv.paragraph(_BuildItemHTML(item, width, display_single));
+const RenderItem = async (item, options) => dv.paragraph(_BuildItemHTML(item, options));
 
 /**
  * 渲染物品
  * @params {Array|Object} items 需要渲染的物品
  * @params {Number} width 图标图片尺寸, 默认值: 15
- * @params {Boolean} display_single 是否显示数量1的数量值, 默认值: false
+ * @params {Number} display_num 显示数量的下限, 大于次值的数量会显示, 默认值: 1
  * @params {Boolean} inline 多个物品是否输出到一行, 默认值: false
  * @params {String} seperator 多个物品输出到一行时使用的分隔符, 默认值: " , "
  */
-const { items, width=15, display_single=false, inline=false, separator=' , ' } = input;
+const { 
+   items,
+   options
+} = input;
+const DefaultOptions = {
+   width: 15,
+   display_num: 1,
+   inline: false,
+   separator: ' , ',
+   fields: {
+      name: 'Name',
+      icon: 'Icon',
+      num: 'Num',
+   }
+};
+const MergedOptions = Object.assign(DefaultOptions, options);
+console.log(MergedOptions);
 Array.isArray(items)
-   ? await Renderitems(items, width, display_single, inline, separator)
-   : await RenderItem(items, width, display_single);
+   ? await Renderitems(items, MergedOptions)
+   : await RenderItem(items, MergedOptions);
