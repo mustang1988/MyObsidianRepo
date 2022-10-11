e4bc6ffd5d4418459ad29a622ee2dca7a6c335fe
mustang1988 <mustang1988@126.com>
2022-10-11 19:44:47 +0800

    refactor: refactor potion item display script

--- Views/Item/Potion/Admonition/view.js
+++ Views/Item/Potion/Admonition/view.js
@@ -1,7 +1,40 @@
 const DEBUG = true;
 const DEFAULT_OPTIONS = {
-  size: 15,
+  collapse: "open",
   raw: true,
-  db: ""
+  db: "游戏攻略/英雄传说 闪之轨迹/数据库/消耗品",
 };
-let {} = input;
+const MergeOptions = (options) => Object.assign(DEFAULT_OPTIONS, options);
+const GetPotion = (id, db) => {
+  const [potion = null] = dv.page(db).Potions.filter((p) => p.ID === id);
+  if (potion === null) {
+    return {
+      potion: null,
+      link: null,
+    };
+  }
+  return {
+    potion,
+    link: dv.blockLink(db, id, false, potion.Name),
+  };
+};
+let { potion, options } = input;
+options = MergeOptions(options);
+DEBUG &&
+  console.debug(
+    "[消耗品Admonition渲染][Views/Item/Potion/Adminition/view.js][Input]:\n",
+    { potion, options }
+  );
+const { ID } = potion;
+const { raw, db, collapse } = options;
+const { potion: potionData, link: potionLink } = GetPotion(ID, db);
+if (potionData === null) {
+  return "";
+}
+const { Range, Effects } = potionData;
+const adm = `\`\`\`ad-Potion
+title: ${potionLink}
+collapse: ${collapse}
+${Range} ${Effects.join(" ")}
+\`\`\``;
+return raw ? adm : dv.span(adm);
--- Views/Item/Potion/InLine/view.js
+++ Views/Item/Potion/InLine/view.js
@@ -1,2 +1,61 @@
 const DEBUG = true;
-let {} = input;
+const DEFAULT_OPTIONS = {
+  size: 15,
+  raw: true,
+};
+const POTION_ICON_KEY = "Item.Potion";
+const MergeOptions = (options) => Object.assign(DEFAULT_OPTIONS, options);
+const GetPotion = (link) => {
+  const { path, subpath } = link;
+  const [potion = null] = dv.page(path).Potions.filter((p) => p.ID === subpath);
+  if (potion === null) {
+    return { potion: null, link: null };
+  }
+  return {
+    potion,
+    link: dv.blockLink(path, subpath, false, potion.Name),
+  };
+};
+const GetSize = (width, height, targetSize) => {
+  return Math.round((width * targetSize) / height);
+};
+
+const GetSource = (file) => {
+  return `${this.app.vault.adapter.basePath}/${file.path}`;
+};
+const GetPotionIcon = async (size) => {
+  return dv
+    .view("Icons/Icon", {
+      key: POTION_ICON_KEY,
+      options: { raw: true },
+    })
+    .then((icon) => {
+      if (icon === null) {
+        return "";
+      }
+      const { File: file, Width: width, Height: height } = icon;
+      return `<img src="${GetSource(file)}" width="${GetSize(
+        width,
+        height,
+        size
+      )}"/>`;
+    });
+};
+// ===== Begin =====
+let { link, options } = input;
+options = MergeOptions(options);
+DEBUG &&
+  console.debug(
+    "[消耗品InLine渲染][Views/Item/Potion/Inline/view.js][Input]:\n",
+    { link, options }
+  );
+
+const { size, raw } = options;
+const inline = GetPotionIcon(size).then((icon) => {
+  if (icon === "") {
+    return "";
+  }
+  const { link: potionLink } = GetPotion(link);
+  return `${icon} ${potionLink}`;
+});
+return raw ? inline : dv.span(inline);
