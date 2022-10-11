32fa991d79a0af2e048d4b4e926be70ea9ad88d8
mustang1988 <mustang1988@126.com>
2022-10-11 14:08:18 +0800

    refactor: refactor scripts

--- Views/Admonition/BookAdmonition.js
+++
@@ -1,8 +0,0 @@
-const { book, options } = input;
-const { Content, Name } = book;
-const { collapse = true } = options;
-return `\`\`\`ad-Book
-title: 《${Name}》
-collapse: ${collapse ? "open" : "close"}
-${Content}
-\`\`\``;
