5e2a33dbf7ba2250b3ebf3f7f1ae7e373eb6ddb2
mustang1988 <mustang1988@126.com>
2022-10-11 13:32:48 +0800

    feat: add link title for book admonition in script

--- Views/Item/Book/Admonition/view.js
+++ Views/Item/Book/Admonition/view.js
@@ -1,9 +1,20 @@
 const DEBUG = false;
 const DEFAULT_OPTIONS = {
   collapse: "open",
+  db: "游戏攻略/英雄传说 闪之轨迹/数据库/书籍",
   raw: true,
 };
 const MergeOptions = (options) => Object.assign(DEFAULT_OPTIONS, options);
+const GetBook = (id, db) => {
+  const [book = null] = dv.page(db).Books.filter((b) => b.ID === id);
+  if (book === null) {
+    return { book, link: null };
+  }
+  return {
+    book,
+    link: dv.blockLink(db, id, false, `《${book.Name}》`),
+  };
+};
 // ===== Begin =====
 let { book, options } = input;
 options = MergeOptions(options);
@@ -12,10 +23,19 @@ DEBUG &&
     "[书籍Admonition渲染][Views/Item/Book/Admonition/view.js][Input]:\n",
     { book, options }
   );
-const { Content, Name } = book;
-const { raw, collapse } = options;
+const { raw, collapse, db } = options;
+const { book: bookData, link } = GetBook(book.ID, db);
+DEBUG &&
+  console.debug(
+    "[书籍Admonition渲染][Views/Item/Book/Admonition/view.js][bookData]:\n",
+    bookData
+  );
+if (bookData === null) {
+  return "";
+}
+const { Content } = bookData;
 const adm = `\`\`\`ad-Book
-title: 《${Name}》
+title: ${link}
 collapse: ${collapse}
 ${Content}
 \`\`\``;
