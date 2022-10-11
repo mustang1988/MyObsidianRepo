fecd9431f5e2da9574ef14cbf2accc346fdccff6
mustang1988 <mustang1988@126.com>
2022-10-11 20:12:37 +0800

    docs: add food db doc file

--- Views/Admonition/Food/Ingredient.js
+++
@@ -1,5 +0,0 @@
-const { item, options } = input;
-const { Name } = item;
-return `\`\`\`ad-Food-Ingredient
-title: ${Name}
-\`\`\``;
--- Views/Item/Food/Superb/Admonition/view.js
+++ Views/Item/Food/Superb/Admonition/view.js
@@ -1,42 +1,45 @@
 const DEBUG = true;
 const DEFAULT_OPTIONS = {
-    collapse: "open",
-    raw: true
-}
+  collapse: "open",
+  raw: true,
+  db: "游戏攻略/英雄传说 闪之轨迹/数据库/食物",
+};
 
 const MergeOptions = (options) => {
-    return Object.assign(DEFAULT_OPTIONS, options);
+  return Object.assign(DEFAULT_OPTIONS, options);
 };
 
-const BuildAdmonition = (food, options) => {
-    const { Name, ID, Effects, Range, Cook = null, path="游戏攻略/英雄传说 闪之轨迹/数据库/物品" } = food;
-    const { collapse } = options;
-    const adType = `ad-Food-Superb`;
-    const foodLink = dv.blockLink(path, ID, false, Name);
-    const effectList = dv.markdownList(Effects);
-    const cookList = Cook !== null
-        ? `料理人: \n${dv.markdownList(Cook)}`
-        : ''
-    return `~~~${adType}
-title: ${foodLink}
-collapse: ${collapse}
-
-范围: 
-${Range}
-
-效果: 
-${effectList}
-
-${cookList}
-
-~~~`;
+const GetAttackFood = (id, db) => {
+  const [food = null] = dv.page(db).Foods.filter((f) => f.ID === id);
+  if (food === null) {
+    return { food: null, link: null };
+  }
+  return {
+    food,
+    link: dv.blockLink(db, id, false, food.Name),
+  };
 };
 
+// ===== Begin =====
 let { food, options } = input;
 options = MergeOptions(options);
-DEBUG && console.debug(`[${__filename}][Input]: `, { food, options });
-const { raw } = options;
-const admonition = BuildAdmonition(food, options)
-return raw
-    ? admonition
-    : dv.span(admonition);
+DEBUG &&
+  console.debug(
+    "[极品食物Admonition渲染][Views/Item/Food/Superb/Admonition][Input]:\n",
+    { food, options }
+  );
+const { db, raw, collapse } = options;
+const { ID } = food;
+const { food: foodData, link: foodLink } = GetAttackFood(ID, db);
+if (food === null) {
+  return "";
+}
+const { Effects, Range, Cook = null } = foodData;
+const adm = `\`\`\`ad-Food-Superb
+title: ${foodLink}
+collapse: ${collapse}
+${Range} ${Effects.join(" ")}
+
+${Cook ? Cook.join(", ") : ""}
+\`\`\``;
+return raw ? adm : dv.span(adm);
