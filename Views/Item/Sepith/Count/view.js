27952eaa1e1b73d25e2b3aadcb4fa7b3129afd45
顾鹏 <pgu@landhightech.com>
2022-10-14 11:49:22 +0800

    feat: add sepith inline with count display script

---
+++ Views/Item/Sepith/Count/view.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+// ===== Constrains =====
+const DEBUG = true;
+const DEFAULT_OPTIONS = {
+    size: 15,
+    count: 0,
+    linit: 0,
+    display_name: false,
+    raw: true,
+};
+
+// ===== Functions =====
+const MergeOptons = () => Object.assign(DEFAULT_OPTIONS, options);
+
+// ===== Begin =====
+let { link, options } = input;
+options = MergeOptons(options);
+DEBUG && console.debug("[耀晶片带数量InLine渲染][Views/Item/Sepith/Count/view.js][Input]:\n", { link, options });
+const inline = dv.view("Item/Sepith/InLine", { link, options }).then(inline => {
+    if(inline === '') {
+        return '';
+    }
+    const { count, limit } = options;
+    if(count > limit) {
+        return `${inline} x ${count}`
+    }
+    return inline;
+});
+const { raw } = options;
+return raw ? inline : dv.span(raw);
