ed3e49138fd82929a5c109c87bbfd8fc821df7f0
顾鹏 <pgu@landhightech.com>
2022-10-18 19:17:46 +0800

    feat: add scripts for character and monster db in sen no kiseki

--- Views/Page/Character/view.js
+++ Views/Page/Character/view.js
@@ -11,6 +11,6 @@ const DEFAULT_OPTIONS = {
 const MergeOptons = (options) => Object.assign(DEFAULT_OPTIONS, options);
 
 // ===== Begin =====
-let { monster, options } = input;
+let { character,options } = input;
 options = MergeOptons(options);
-DEBUG && console.debug("[][Views/Page/Monster/view.js][Input]:\n", { monster, options });
+DEBUG && console.debug("[][Views/Page/Character/view.js][Input]:\n", { character,options });
